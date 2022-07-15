e37d15e88732b7a50a5baf9f9e008133fc134930
mohitchandak <mohitchandak100@gmail.com>
2022-07-15 22:48:07 +0530

    first commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
   return (
     <div className="py-5 border-t-3/2">
             <div className="flex align-center justify-center mt-4">
-              <a target="_blank" rel="noreferrer" className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-gray-800 hover:bg-gray-800 rounded-full hover:text-white transition-colors duration-300" href="https://github.com/mohitchandak">
+              <a target="_blank" rel="noreferrer"   className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-gray-800 hover:bg-gray-800 rounded-full hover:text-white transition-colors duration-300" href="https://github.com/mohitchandak">
                 <FaGithub />
                 <span class="sr-only">Github</span>
               </a>
