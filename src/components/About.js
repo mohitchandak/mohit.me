3fa849dc58734c92a3369b0ec6ac926557c3bb2d
Mohit Chandak <65466004+mohitchandak@users.noreply.github.com>
2023-01-11 16:20:54 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -17,7 +17,7 @@ function About() {
           take part in hackathons or related stuff.
         </p>
         <div className="text-center m-auto">
-          <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1PS5ak3mj3xnHgCkVd9Z8xY38LKXD0eIw/view?usp=share_link">
+          <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1dPo4ftkEc0PwEOZu3NPKEZnXEGY2lSTe/view?usp=share_link">
             <span
               className="mt-8 inline-block w-auto bg-black font-display text-white text-base md:text-xl py-3 px-5 cursor-pointer"
             >
